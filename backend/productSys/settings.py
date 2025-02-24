aa0174bc79b4c34940302d4c251958b21c043d1b
HAOOO99 <xuyanhao2000@gmail.com>
2025-02-25 10:52:18 +1100

    update database setting

--- backend/productSys/settings.py
+++ backend/productSys/settings.py
@@ -101,27 +101,27 @@ CHANNEL_LAYERS = {
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'mysql.connector.django',
-        'NAME': 'system',
-        'USER': 'root',
-        'PASSWORD': 'King123',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': '',
-#         'NAME': 'railway',
+#         'ENGINE': 'mysql.connector.django',
+#         'NAME': 'system',
 #         'USER': 'root',
-#         'PASSWORD': 'SutypJRbeXHQnVNpxpHLnCYXXWVIUOnx',
-#         'HOST': 'mysql.railway.internal',
+#         'PASSWORD': 'King123',
+#         'HOST': '127.0.0.1',
 #         'PORT': '3306',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'SutypJRbeXHQnVNpxpHLnCYXXWVIUOnx',
+        'HOST': 'mysql.railway.internal',
+        'PORT': '3306',
+    }
+}
 
 
 # Password validation
