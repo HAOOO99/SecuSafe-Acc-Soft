145c343a54242dad87159073a85220d54634f8c1
HAOOO99 <xuyanhao2000@gmail.com>
2025-03-03 11:39:08 +1100

    change server for production setting

--- frontend/acc-soft-website/src/Components/PIPage.jsx
+++ frontend/acc-soft-website/src/Components/PIPage.jsx
@@ -7,7 +7,7 @@ import NavBar from './NavBar';
 import { useParams } from 'react-router-dom';
 
 import { w3cwebsocket as W3CWebSocket } from 'websocket';
-const client = new W3CWebSocket('wss://secusafe-acc-soft-production.up.railway.app/ws/comments/');
+const client = new W3CWebSocket('wss://secusafe-acc-soft-production.up.railway.app/comments/');
 
 
 export default function PIPage(){
