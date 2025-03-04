88323e7643a574d67c392a492afbcb358f6275f2
HAOOO99 <xuyanhao2000@gmail.com>
2025-03-04 16:01:20 +1100

    add django redis installment

--- backend/productSys/settings.py
+++ backend/productSys/settings.py
@@ -168,7 +168,7 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 STATIC_URL = 'static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
